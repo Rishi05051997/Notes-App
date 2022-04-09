ae09297b59a4b002b9651b588e35c5b1cede402e
rishi05051997 <vrushabhdhatral10@gmail.com>
2022-04-09 13:55:32 +0530

    Feat : Some Changes in Layout

--- src/components/Note/AddNote.jsx
+++ src/components/Note/AddNote.jsx
@@ -1,38 +1,67 @@
-import { useData } from "../../context/DataProvider"
-
 export const AddNote = () => {
-    const { isExpanded } = useData();
     return (
-        <section className={isExpanded ? "note-main-content" : "note-main-content-collasped"}>
-            <div className="note-content">
-                <div className="container_note">
-                    <div className="head-1 highlightMainText">My Notes App</div>
-                    <div className="form container-card xxl-card-width pad-md">
-                        <div className="custom-input-one mar-y-2">
-                            <input
-                                id="note-title"
-                                type="text"
-                                className="input-field"
-                                autocomplete="off"
-                                placeholder=" "
-                            />
-                            <label for="note-title" className="input-label text-2"
-                            >Note Title</label
-                            >
-                        </div>
-                        <textarea
-                            id="note-text"
-                            className="custom-input-one mar-y-2"
-                            placeholder="Note Details"
-                        ></textarea>
-
-                        <div className="mar-y-3">
-                            <button className="btn btn-primary" id="add-btn">Add Note</button>
-                        </div>
+        <>
+            <div class="modal">
+                <div class="modal-container lg-modal-width mar-y-2">
+                    <div class="modal-header pad-xs">
+                        <div class="head-2 highlightMainText">Add New Note</div>
+                        <span
+                            class="iconify modal-close text-3 highlightMainText cursor_"
+                            data-icon="ant-design:close-circle-outlined"
+                        ></span>
                     </div>
+                    <form >
+                        <div class="modal-body text-2 pad-xs">
+                            <div className="custom-input-one mar-y-2">
+                                <input
+                                    id="note-title"
+                                    type="text"
+                                    className="input-field"
+                                    autocomplete="off"
+                                    placeholder=" "
+                                />
+                                <label for="note-title" className="input-label text-2"
+                                >Note Title</label
+                                >
+                            </div>
+                            <textarea
+                                id="note-text"
+                                className="custom-input-one mar-y-2"
+                                placeholder="Note Details"
+                                rows={10}
+                                cols={10}
+
+                            >
+
+                            </textarea>
+
+
+
+                            <div className="custom-input-one mar-y-2">
+                                <input
+                                    id="note-title"
+                                    type="text"
+                                    className="input-field"
+                                    autocomplete="off"
+                                    placeholder=" "
+                                />
+                                <label for="note-title" className="input-label text-2"
+                                >Add Lebels</label
+                                >
+                            </div>
+
+                        </div>
+
+
+                        <div className="modal-footer pad-sm">
+                            <button className="btn btn-primary">
+                                Add Note
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
-
-        </section>
+            <div class="overlay"></div>
+        </>
     )
 }
--- src/components/Note/Sidebar.jsx
+++ src/components/Note/Sidebar.jsx
@@ -3,10 +3,12 @@ import { Link, useNavigate } from "react-router-dom"
 import { useAuth } from "../../context/AuthProvider";
 import { useData } from "../../context/DataProvider";
 import { logOutUser } from "../../services";
+import { AddNote } from "./AddNote";
 export const Sidebar = ({ login }) => {
-    const { isExpanded, setIsExpanded } = useData();
+    const { state: { isModelOpen }, dispatch, isExpanded, setIsExpanded } = useData();
     const navigate = useNavigate();
     const { setLogin } = useAuth();
+    console.log(isModelOpen)
 
     const handleToggler = () => {
         if (isExpanded) {
@@ -24,9 +26,16 @@ export const Sidebar = ({ login }) => {
                 <div className={isExpanded ? "sidebar" : "sidebar collapsed"}>
                     <header className="sidebar-header">
                         <div className="sidebar-icon text-4">
-                            <Icon className="iconify" onClick={handleToggler} icon="carbon:menu" />
+                            <Icon className="iconify highlightMainText" onClick={handleToggler} icon="carbon:menu" />
                         </div>
-                        <h1 className="sidebar-logo head-1">NOTE APP</h1>
+                        <h1 className="sidebar-logo head-1 highlightMainText">NOTE APP</h1>
+                        <nav className="siderbar-navigation">
+                            <button className="btn btn-primary" onClick={() => dispatch({ type: "TOGGLE-MODDAL" })}>Create Note</button>
+                            <div className="badge-div highlightMainText">
+                                <Icon className="iconify" icon="healthicons:ui-user-profile" />
+                                <div className="text-2">User Name</div>
+                            </div>
+                        </nav>
                     </header>
                     <main className="sidebar-items">
                         <div className="item">
@@ -61,7 +70,28 @@ export const Sidebar = ({ login }) => {
                         </div>
                     </main>
                 </div>
+                <section className={isExpanded ? "note-main-content" : "note-main-content-collasped"}>
+                    <header className="content-header">
+                        <div className="sort-by-field" >
+                            <label htmlFor="sortBy" className="head-3">Sort By</label>
+                            <select name="" id="" className="head-3">
+                                <option value="">Newest</option>
+                                <option value="">Oldest</option>
+                            </select>
+                        </div>
+                        <nav className="content-nav text-3 bold">
+                            <div>Priority</div>
+                            <div className="priority-round cursor_">High</div>
+                            <div className="priority-round cursor_">Medium</div>
+                            <div className="priority-round cursor_">Low</div>
+                        </nav>
+                    </header>
+                </section>
+
             </div>
+            {
+                isModelOpen && <AddNote />
+            }
         </>
     )
 }
